cdea6c30504af17522a275bd51bddef9b4757a08
Paul Harten <Harten.Paul@epa.gov>
2023-08-24 09:03:52 -0400

    Using powers of 2 rather than powers of 10.

--- jscriptREPL.jl
+++ jscriptREPL.jl
@@ -9,7 +9,7 @@ include("src\\parallel_pi.jl")
 #define julia_p as "julia -p 1" that is "-p 1" as argument to the interpreter "julia"
 #the above seems to be necessary to accept the parallel macros
 
-@time ppi, uncert = parallel_pi(10^11, ncores)
+@time ppi, uncert = parallel_pi(2^40, ncores)
 
 println(pi," = ",pi+0.0,", ppi = ",ppi," +/- ",uncert,", deltaPi = ",abs(pi-ppi))
 
